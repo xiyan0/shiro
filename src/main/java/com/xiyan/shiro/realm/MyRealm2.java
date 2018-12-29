b2855560e4abdb35f0dd12ccd1e5dda52db11558
xiyan0 <351523513@qq.com>
2018-12-29 09:52:25 +0800

    Initial commit

---
+++ src/main/java/com/xiyan/shiro/realm/MyRealm2.java
@@ -0,0 +1,36 @@
+package com.xiyan.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm2 implements Realm {
+
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		String username = (String) token.getPrincipal(); // 得到用户名
+		String password = new String((char[]) token.getCredentials()); // 得到密码
+		if (!"lisi".equals(username)) {
+			throw new UnknownAccountException(); // 如果用户名错误
+		}
+		if (!"123".equals(password)) {
+			throw new IncorrectCredentialsException(); // 如果密码错误
+		}
+		// 如果身份认证验证成功，返回一个AuthenticationInfo实现；
+		return new SimpleAuthenticationInfo(username, password, getName());
+	}
+
+	public String getName() {
+		return "myrealm1";
+	}
+
+	public boolean supports(AuthenticationToken token) {
+		// 仅支持UsernamePasswordToken 类型的Token
+		return token instanceof UsernamePasswordToken;
+	}
+
+}
